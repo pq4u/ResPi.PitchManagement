029d925a36ae41d3263402f9164a120c3fcf5a9f
pawtes <paweltes19@gmail.com>
2021-06-14 22:33:46 +0200

    Add project files.

---
+++ ResPi.PitchManagement.Domain/Entities/Event.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ResPi.PitchManagement.Domain.Common;
+
+namespace ResPi.PitchManagement.Domain.Entities
+{
+    public class Event : AuditableEntity
+    {
+        public Guid EventID { get; set; }
+        public string Name { get; set; }
+        public string Owner { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public string Phone { get; set; }
+        public Guid CategoryID { get; set; }
+        public Category Category { get; set; }
+    }
+}
---
+++ ResPi.PitchManagement.Domain/Entities/Order.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ResPi.PitchManagement.Domain.Common;
+
+namespace ResPi.PitchManagement.Domain.Entities
+{
+    public class Order : AuditableEntity
+    {
+        public Guid Id { get; set; }
+        public Guid UserID { get; set; }
+        public int OrderTotal { get; set; }
+        public DateTime OrderPlaced { get; set; }
+        public bool OrderPaid { get; set; }
+    }
+}
