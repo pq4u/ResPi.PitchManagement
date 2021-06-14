48a805c6228ce86e58cf86fbb9a08852fbfc676b
pawtes <paweltes19@gmail.com>
2021-06-14 22:42:36 +0200

    Add persistence

---
+++ ResPi.PitchManagement.Application/Contracts/Persistence/IOrderRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ResPi.PitchManagement.Domain.Entities;
+
+namespace ResPi.PitchManagement.Application.Contracts.Persistence
+{
+    public interface IOrderRepository : IAsyncRepository<Order>
+    {
+    }
+}
