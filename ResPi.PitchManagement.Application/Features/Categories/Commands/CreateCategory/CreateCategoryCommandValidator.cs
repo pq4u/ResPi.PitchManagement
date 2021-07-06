92a578856bedb1cd2d7546c9cba76063b5048c27
pawtes <paweltes19@gmail.com>
2021-07-06 14:52:31 +0200

    Add create category command and validation

---
+++ ResPi.PitchManagement.Application/Features/Categories/Commands/CreateCategory/CreateCategoryCommandValidator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using FluentValidation.Validators;
+
+namespace ResPi.PitchManagement.Application.Features.Categories.Commands.CreateCategory
+{
+    public class CreateCategoryCommandValidator : AbstractValidator<CreateCategoryCommand>
+    {
+        public CreateCategoryCommandValidator()
+        {
+            RuleFor(p => p.Name)
+                .NotEmpty().WithMessage("{PropertyName} cannot be empty.")
+                .NotNull()
+                .MaximumLength(50)
+                .WithMessage("{PropertyName} must not exceed 50 characters.");
+        }
+    }
+}
---
+++ ResPi.PitchManagement.Application/Responses/BaseResponse.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResPi.PitchManagement.Application.Responses
+{
+    public class BaseResponse
+    {
+        public BaseResponse()
+        {
+            Success = true;
+        }
+
+        public BaseResponse(string message = null)
+        {
+            Success = true;
+            Message = message;
+        }
+
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public List<string> ValidationErrors { get; set; }
+    }
+}
